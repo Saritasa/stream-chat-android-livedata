645229506bfdac92f34c8581522902b8f499b044
Maxim Maximov <maxmaxandr@gmail.com>
2020-05-20 15:45:58 +0700

    Add Jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,32 @@
+#!/usr/bin/env groovy
+
+node('android') {
+    try {
+        GRADLE_HOME = tool(name: 'gradle-latest', type: 'gradle')
+        JAVA_HOME = tool(name: 'jdk-latest', type: 'jdk')
+
+        stage('clean up') {
+            cleanWs()
+        }
+
+        stage('scm') {
+            checkout(scm)
+        }
+
+        stage('build and publish') {
+            sh("chmod +x gradlew")
+            sh("./gradlew clean assemble --stacktrace")
+            sh("./gradlew :livedata:uploadArchives --stacktrace")
+        }
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        println("ERROR: ${error}")
+        emailext(
+                subject: "Build - ${currentBuild.currentResult}: ${JOB_NAME}: ${BUILD_NUMBER}",
+                body: "\
+      <h1 style='text-align:center;color:#ecf0f1;background-color:#ff5252;border-color:#ff5252'>${currentBuild.currentResult}</h1>\
+      <b>Job url</b>: ${BUILD_URL}<br>",
+                recipientProviders: [requestor(), developers(), brokenBuildSuspects(), brokenTestsSuspects()]
+        )
+    }
+}
